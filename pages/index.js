9bee48df46fe7109533b7b49c340c9c9a4cf905b
Andre Bury <andre.bury@live.com>
2020-11-11 20:04:20 -0300

    Notificação de Preço e reparos em CSS

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ import {orderBy,findIndex} from 'lodash';
 
 export default function Home() {
   const [ilhas,setIlhas] = useState([])
+  const [idealPriceT,setIdealPriceT] = useState('')
+  const [idealPrice,setIdealPrice] = useState(0)
+  const [dateRefesh,setDateRefesh] = useState('')
+  const [melhorPreco,setMelhorPreco] = useState(0)
 
   function notifica(notificacao) {
     // Verifica se o browser suporta notificações
@@ -32,31 +36,71 @@ export default function Home() {
     }
   }
 
-  useEffect(() => {
-    async function listaIlhas(){
-      await api.post('/islands').then(resp => {
-        setIlhas(orderBy(resp.data.islands, ['turnipPrice'], ['desc']))
-        console.log(ilhas)
-      })
-    if(_.findIndex(ilhas, function(o) { return o.turnipPrice >= 650; })>=0){
-      notifica("Encontrada Ilha")
-      }
+  function trataData(){
+    var data = new Date()
+    var dia = data.getDate()
+    var mes = data.getMonth() + 1
+    var ano = data.getFullYear()
+    var hora = data.getHours()  < 10 ? ('0' + data.getHours()) : data.getHours()
+    var minuto = data.getMinutes() < 10 ? ('0' + data.getMinutes()) : data.getMinutes()
+    var segundo = data.getSeconds() < 10 ? ('0' + data.getSeconds()) : data.getSeconds()
 
-    }
+
+      setDateRefesh(dia + '/' + mes + '/' + ano + ' ' + hora + ':' + minuto + ':' + segundo)
+  }
+
+
+  async function listaIlhas(){
+      
+    trataData()
+
+    await api.post('/islands').then(resp => {
+      const ilhaClassific = orderBy(resp.data.islands, ['turnipPrice'], ['desc'])
+      setIlhas(ilhaClassific)
+      
+      const melhorp = ilhaClassific[0].turnipPrice
+      
+      setMelhorPreco(melhorp)
+
+      if((melhorp >= localStorage.getItem('idealPrice'))){
+        notifica("Encontrada Ilha: " + ilhaClassific[0].name + ', preço ' + melhorp + ' Bells')
+        
+        }
+    })
+  }
+
+
+  useEffect(() => {   
+    setIdealPrice(localStorage.getItem('idealPrice'))
     listaIlhas()
-    setTimeout(function() {
-      listaIlhas()
-      if(_.findIndex(ilhas, function(o) { return o.turnipPrice >= 650; })>=0){
-        notifica("Encontrada Ilha")
+    
+    setInterval(function() {
+        listaIlhas()
       }
+    , 30000);
 
-    }, 30000);
   },[])
+
+  function ShowIdealPrice(){
+    if(idealPrice != null ){
+    return(
+      <p>Acompanhar Ilhas com preço maior que {idealPrice}</p>
+      
+    )
+  }else{
+      return <div></div>
+  }
+  }
+  function handlePrice(){
+    setIdealPrice(idealPriceT)
+    localStorage.setItem('idealPrice',idealPriceT)
+  }
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>TE Islands</title>
+        <title>TE Islands - Melhor Preço: </title>
+        {/* {melhorPreco} */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -64,6 +108,11 @@ export default function Home() {
         <h1 className={styles.title}>
         TE Islands
         </h1>
+        <div className={styles.input}>
+          <input onChange={(e)=>(setIdealPriceT(e.target.value))} value={idealPriceT} placeholder="Coloque o preço ideal"></input>
+          <button onClick={handlePrice}>Salvar</button>
+        </div>
+        <ShowIdealPrice></ShowIdealPrice>
         <div className={styles.grid}>
           {ilhas.map((ilha) => (
             <div className={styles.card}>
@@ -85,6 +134,9 @@ export default function Home() {
 
 
       </main>
+      <footer className={styles.footer}>
+        {dateRefesh}
+      </footer>
 {/* 
       <footer className={styles.footer}>
         <a
